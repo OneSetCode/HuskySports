e598d7d8953fac1e71c77b75e0288c7c45fb8d40
OneSetCode <96799944+OneSetCode@users.noreply.github.com>
2023-06-14 17:17:01 -0700

    Revert "asd"
    This reverts commit 38f040c5f1dc7fd5ea8827c012d27b60edaafb18.

--- Sportsbud/settings.py
+++ Sportsbud/settings.py
@@ -92,28 +92,28 @@ WSGI_APPLICATION = 'Sportsbud.wsgi.application'
 # }
 
 # ---- Default SQLite database
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # ---- Migrate to Heroku Postgres database
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
 
-# Mongo databse (connect via djongo)
-DATABASES = {
-     'default': {
-         'ENGINE': 'djongo',  
-         'NAME': 'mydatabase',
-         'ENFORCE_SCHEMA': False, 
-         'CLIENT': {
-                'host': 'mongodb+srv://mongouser:cLV7U5qPX5txUIw0@cluster0.okaerau.mongodb.net/?retryWrites=true&w=majority'
-            } 
-     }
- }
+# ---- MySQL databse (pymysql, imported in _init_.py)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': os.getenv("DBName"),
+#         'HOST': os.getenv("Host"), 
+#         'PORT': os.getenv("Port"), 
+#         'USER': os.getenv("UserID"),  
+#         'PASSWORD': os.getenv("Password"), 
+#     }
+# }
  
 
 # Password validation
@@ -167,7 +167,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['https://web-production-dfdf.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1','web-production-dfdf.up.railway.app','https://web-production-dfdf.up.railway.app']
 
 
 # --- S3 BUCKETS CONFIGURE
